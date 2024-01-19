905f8911898f02801c0faff0397ade5785637d4b
Chad Misa <chad@matcherino.com>
2024-01-19 16:02:10 +0800

    Remove unused files and added meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,30 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const title = "Chad E. Misa - Front-end Developer";
+  const description = `A passionate and innovative front-end developer on a mission to
+  transform ideas into captivating digital experiences. With a blend of
+  creativity and technical prowess, I thrive in the ever-evolving
+  landscape of web development.`;
+
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width" />
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
+        <meta
+          name="keywords"
+          content="Front-end Developer, React.JS, ReactJS, Next.js, NextJS, HTML, CSS"
+        />
+        <meta name="author" content="Chad E. Misa"></meta>
+
+        <meta property="og:url" content="https://chadmisa-cv.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content="/cv-img.jpg" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
